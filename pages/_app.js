0e9e9037c1ddf2d8c42c698f561bd65d4793f867
raymondtju <raymond.rtju@gmail.com>
2023-03-01 10:54:55 +0700

    add toast container to root

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import "../styles/main.css";
+import { ToastContainer } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <ToastContainer position="top-right" />
+    </>
+  );
 }
