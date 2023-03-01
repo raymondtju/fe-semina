dfb316c183816dd09c37812fb7c1a2e63aadc27a
raymondtju <raymond.rtju@gmail.com>
2023-03-01 10:54:12 +0700

    utlils for axios and format date

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -1,5 +1,4 @@
 import axios from "../configs";
-import Cookies from "js-cookie";
 
 export function getData(url, params, token) {
   return axios.get(`${url}`, {
@@ -8,4 +7,28 @@ export function getData(url, params, token) {
     //   Authorization: `Bearer ${token}`,
     // },
   });
+}
+
+export async function postData(url, payload, token) {
+  try {
+    return await axios.post(`${url}`, payload, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  } catch (err) {
+    return err;
+  }
+}
+
+export async function putData(url, payload, token) {
+  try {
+    return await axios.put(`${url}`, payload, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  } catch (error) {
+    return error;
+  }
 }
--- utils/formatDate.js
+++ utils/formatDate.js
@@ -1,10 +1,12 @@
 export function formatDate(date) {
   const d = new Date(date);
+  console.log(date);
   const dtf = new Intl.DateTimeFormat("en", {
     year: "numeric",
     month: "short",
     day: "2-digit",
   });
+  console.log(dtf);
   const [{ value: mo }, , { value: da }, , { value: ye }] =
     dtf.formatToParts(d);
 
